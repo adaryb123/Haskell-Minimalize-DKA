eac2e32859a3040c632fa203cd3f8114ef873d8c
AdamR <adam.rybansky@centrum.sk>
2022-03-11 18:09:27 +0100

    can process command line arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,30 @@
 module Main where
 
 import System.IO  
-import Parser(parseDKA)
+import System.Environment (getArgs)
+import System.Exit (die)
+import MyData
+import MyParser(parseDKA)
 
-main = do 
-    contents <- readFile "../test/test1input.txt"  
-    let contentString = contents
-    print $ parseDKA contentString
+-- main = do 
+--     contents <- readFile "../test/test1input.txt"  
+--     let contentString = contents
+--     print $ parseDKA contentString
+
+main :: IO ()
+main = do
+    (action, inputFile) <- procArgs =<< getArgs
+    either die action (parseDKA inputFile)
+
+-- Zpracování příkazového řádku
+procArgs :: [String] -> IO (DKA -> IO (), String)
+procArgs [x,y] = do
+    inputFile <- readFile y
+    case x of
+     "-i" -> return (printDKA, inputFile)
+     "-s" -> return (printDKA, inputFile)
+     _    -> die "Error: expecting two arguments: [-i|-s] FILENAME"
+procArgs _ = die "Error: expecting two arguments: [-i|-s] FILENAME"
+
+printDKA :: DKA -> IO ()
+printDKA = putStr . show
