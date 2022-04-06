41712222c3df2f4e46de7bd0bef4cd1fb0f866fb
AdamR <adam.rybansky@centrum.sk>
2022-04-06 17:12:16 +0200

    main.hs refactor

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,7 @@
 -- Login : xryban00
 
 module Main where
-
-import System.IO  
+ 
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import MyData
@@ -14,21 +13,21 @@ import MyConversion(convertToMKA)
 --load the arguments
 main :: IO ()
 main = do
-    (action, inputFile) <- processArgs =<< getArgs
+    (action, inputFile) <- processArguments =<< getArgs
     either terminate action (parseDKA inputFile)
 
 --read the input from file or stdin
-processArgs :: [String] -> IO (DKA -> IO (), String)
-processArgs [option] = processOptions option =<< getContents
-processArgs [option, inputFile] = processOptions option =<< readFile inputFile
-processArgs _ = terminate "Excepting an option argument and optionally an input file: flp21-fun (-i|-t) [input-file]"
+processArguments :: [String] -> IO (DKA -> IO (), String)
+processArguments [option] = executeAction option =<< getContents
+processArguments [option, inputFile] = executeAction option =<< readFile inputFile
+processArguments _ = terminate "Error: unknown arguments\n"
 
 --print the input DKA or the converted MKA
-processOptions :: String -> String -> IO (DKA -> IO (), String)
-processOptions option input = case option of
-  "-i" -> return (printDKA, input)
-  "-t" -> return (convertDKA, input)
-  _ -> terminate $ "Unknown option: " ++ option
+executeAction :: String -> String -> IO (DKA -> IO (), String)
+executeAction option input
+  | option == "-i" = return (printDKA, input)
+  | option == "-t" = return (convertDKA, input)
+  | otherwise = terminate "Error: unknown arguments\n"
 
 --print DKA loaded from file
 printDKA :: DKA -> IO ()
@@ -38,6 +37,6 @@ printDKA = putStr . show
 convertDKA :: DKA -> IO ()
 convertDKA = printDKA . convertToMKA
 
---terminates the program with an error code and an error message
+--end the program and print error message
 terminate :: String -> IO a
-terminate str = hPutStrLn stderr str >> exitFailure
+terminate str = putStr str >> exitFailure
